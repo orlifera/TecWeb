09612eadfd3700a6a097899c0a5e8f44d85a973f
Orlando Ferazzani <orli@Orlandos-MacBook-Air.local>
2023-11-25 00:26:48 +0100

    aggiunto faq-handler. Fixato form-handler e form. Create div Team

--- src/JS/form-handler.js
+++ src/JS/form-handler.js
@@ -9,11 +9,16 @@ window.onload = function () {
     .addEventListener("submit", function (event) {
       event.preventDefault();
       // generate a five digit number for the contact_number variable
-      this.contact_number.value = (Math.random() * 100000) | 0;
+      var contactNumberInput = document.createElement("input");
+      contactNumberInput.type = "hidden";
+      contactNumberInput.name = "contact_number";
+      contactNumberInput.value = (Math.random() * 100000) | 0;
+      this.appendChild(contactNumberInput);
       // these IDs from the previous steps
-      emailjs.sendForm("contact_service", "contact_form", this).then(
+      emailjs.sendForm("service_pklquej", "template_dyacfuz", this).then(
         function () {
           console.log("SUCCESS!");
+          alert("Form inviato correttamente. Vi risponderemo al più presto!");
         },
         function (error) {
           console.log("FAILED...", error);
