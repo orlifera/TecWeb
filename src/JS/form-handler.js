020fbc9d3c34a5b17cdb88a89593cae7498cc65b
Orlando Ferazzani <orli@Orlandos-MacBook-Air.local>
2023-11-24 17:50:03 +0100

    creato file form-handler.js. Finito il form, da sistemare il suo css.

---
+++ src/JS/form-handler.js
@@ -0,0 +1,23 @@
+(function () {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init("iNdbzp60ZWw2gITAZ");
+})();
+
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm("contact_service", "contact_form", this).then(
+        function () {
+          console.log("SUCCESS!");
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+    });
+};
