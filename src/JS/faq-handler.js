6b0823e2c51c66395f3b8af4d6701d1eaa9eaa7f
Davide <davide.dona02@gmail.com>
2023-12-24 12:23:16 +0100

    risolto problema faq.js

--- src/JS/faq-handler.js
+++ src/JS/faq-handler.js
@@ -1,4 +1,33 @@
+function showAnswer(q) { 
+  q.classList.remove("hidden");
+  q.classList.add("show");
+}
+
+function hideAnswer(q) {
+  q.classList.remove("show");
+  q.classList.add("hidden");
+}
+
+function toggleAnswer(event) {
+  console.log("clicked", event.currentTarget);
+  var question = event.currentTarget;
+  var answer = question.nextElementSibling;
+  if (answer.classList.contains("show")) {
+    hideAnswer(answer);
+  } 
+  else if (answer.classList.contains("hidden")) {
+    showAnswer(answer);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
+
+  document.querySelectorAll('.question').forEach(function (question) {
+        question.addEventListener('click', toggleAnswer);
+    });
+
+});
+/*document.addEventListener('DOMContentLoaded', function () {
 
   var questions = document.getElementsByClassName('question');
 
@@ -30,22 +59,4 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
 });
-
-// function toggleAnswer(question) {
-//   // Find the next sibling, which should be the corresponding answer
-//   var answer = question.nextElementSibling;
-
-//   // Toggle the 'display' property of the answer element
-//   if (answer.style.display === "block") {
-//     answer.style.display = "none";
-//   } else {
-//     answer.style.display = "block";
-//   }
-// }
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   const question = document.querySelector('#question');
-//   question.addEventListener('click', function(event) {
-//     toggleAnswer(question);
-//   });
-// });
+*/
