73a191d9d66824fd13268bd8a629dc9a10e30db7
Orlando Ferazzani <orli@Orlandos-MacBook-Air.local>
2023-12-24 10:20:30 +0100

    signup

--- src/JS/signup.js
+++ src/JS/signup.js
@@ -1,4 +1,5 @@
 let signupContent = document.querySelector(".signup-container"),
+    stagebtn1a = document.querySelector(".stagebtn1a"),
     stagebtn1b = document.querySelector(".stagebtn1b"),
     stagebtn2a = document.querySelector(".stagebtn2a"),
     stagebtn2b = document.querySelector(".stagebtn2b"),
@@ -8,5 +9,49 @@ let signupContent = document.querySelector(".signup-container"),
     signupContent2 = document.querySelector(".stage2-content"),
     signupContent3 = document.querySelector(".stage3-content");
 
-signupContent2.classList.add("hidden");
-signupContent3.classList.add("hidden");
+function showStage(...contents) {
+    contents.forEach(contents => {
+        if (contents.classList.contains("hidden"))
+            contents.classList.remove("hidden");
+        contents.classList.add("show");
+    });
+}
+
+function hideStage(...contents) {
+    contents.forEach(contents => {
+        if (contents.classList.contains("show"))
+            contents.classList.remove("show");
+        contents.classList.add("hidden");
+    });
+}
+
+function switchStage(toHide, toShow) {
+    hideStage(toHide);
+    showStage(toShow);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    showStage(signupContent1);
+    hideStage(signupContent2, signupContent3);
+
+
+
+    stagebtn1b.addEventListener('click', function () {
+        switchStage(signupContent1, signupContent2);
+    });
+
+    stagebtn2a.addEventListener('click', function () {
+        switchStage(signupContent2, signupContent1);
+    });
+
+    stagebtn2b.addEventListener('click', function () {
+        switchStage(signupContent2, signupContent3);
+    });
+
+    stagebtn3a.addEventListener('click', function () {
+        switchStage(signupContent3, signupContent2);
+    });
+
+
+});
+
