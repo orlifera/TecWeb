bfed02c431f676332e6b91bebb31cfe0e79b6123
Filippo <rizzolofilippo18@gmail.com>
2024-01-19 17:48:20 +0100

    Fix dell'errore su login

--- src/php/login.php
+++ src/php/login.php
@@ -1,13 +1,15 @@
 <?php
 
 session_start();
-// if (isset($_SESSION['user']) && $_SESSION['user'] !== '') {
-//     header('Location: /pages/profile.html');
-//     exit();
-// }
+$paginaHTML = file_get_contents(__DIR__ . "/../pages/login.html");
+
+if (isset($_SESSION['user']) && $_SESSION['user'] !== '') {
+    header('Location: ../php/profile.php');
+    exit();
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['submit'])) {
+    if (isset($_POST['submit']) && isset($_POST['user']) && isset($_POST['password'])) {
         require_once("connessionedb.php");
 
         $username = htmlentities(substr($_POST['user'], 0, 255));
@@ -21,29 +23,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if ($result->num_rows > 0) {
             $user_data = $result->fetch_assoc();
             if (password_verify($password, $user_data["password"])) {
-                // session_start();
-                // session_regenerate_id();
                 $_SESSION["user"] = $user_data["username"];
-                // print_r($_SESSION);
-                // var_dump($_SESSION);
-                // $_SESSION["password"] = $user_data["password"];
                 if(isset($_SESSION['user'])) {
                     header('Location: ../php/profile.php');
                 } else {
                     echo '<p>Non va la sessione</p>';
-                    // phpinfo();
                 }
                 exit();
             } else {
-                $error = "Password errata";
+                // echo "<div id=\"error\">username o password errati</div>";
+                $errormsg="<div class=\"error\">Email o Password non coincidono</div>";
+                $paginaHTML = str_replace('{errore}', $errormsg, $paginaHTML);
+                echo($paginaHTML);
+                // $_SESSION['error'] = $errormsg;
+                // echo "<script>document.getElementById('error').innerHTML='$errormsg'</script>";
+                // header('Location: ../pages/login.html');
+                // $error = "Password errata";
             }
         } else {
-            $error = "Nome utente non trovato";
+            // echo "<div id=\"error\">username o password errati</div>";
+            
+            
+            
+            // $errormsg="<div class=\"error>\"Email o Password non coincidono</div>";
+            $errormsg="<div class=\"error\">Email o Password non coincidono</div>";
+            $paginaHTML = str_replace('<div id="error"></div>', $errormsg, $paginaHTML);
+            echo($paginaHTML);
+            
+            
+            
+            // $_SESSION['error'] = $errormsg;
+            // echo "<script>document.getElementById('error').innerHTML='$errormsg'</script>";
+            // header('Location: ../pages/login.html');
+            // $error = "Password errata";
+            // if ($errormsg) {
+            //     $error_html = str_replace("{errore}", $errormsg, '<div id="error">{errore}</div>');
+            //     echo $error_html;
+            // }
         }
         $connessione->close();
     }
 }
-
-// UTILIZZO QUESTO PER VEDERE L'ID DELLA SESSIONE SALVATO NEL COOKIE
-// document.cookie
-// document.cookie.split(';')
