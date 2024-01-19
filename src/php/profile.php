4ef508e0cbec3cdfa1b01dfe1e9cbf100472c6ad
Orlando Ferazzani <orli@Orlandos-MacBook-Air.local>
2024-01-19 15:56:47 +0100

    fix palette

--- src/php/profile.php
+++ src/php/profile.php
@@ -28,11 +28,11 @@ $cap = "";
 $connection = new DBAccess();
 $connectionOk = $connection->openDBConnection();
 
-if($connectionOk) {
-    $username = "<h2 id=\"nav-title\">Ciao, " .$user. "!</h2>";
+if ($connectionOk) {
+    $username = "<h2 id=\"nav-title\">Ciao, " . $user . "!</h2>";
     $listaInfo = $connection->getProfileInfo($user);
 
-    if(isset($_POST['personalInfo'])) {
+    if (isset($_POST['personalInfo'])) {
         $fname = $_POST['fname'];
         $lname = $_POST['lname'];
         $email = $listaInfo[5];
@@ -41,7 +41,7 @@ if($connectionOk) {
         header('Location: profile.php');
     }
 
-    if(isset($_POST['address'])) {
+    if (isset($_POST['address'])) {
         $email = $listaInfo[5];
         $city = $_POST['city'];
         $address = $_POST['address'];
@@ -50,13 +50,13 @@ if($connectionOk) {
         header('Location: profile.php');
     }
 
-    if(isset($_POST['changepsw'])) {
-        if($_POST['pwd'] != null && $_POST['password'] != null && $_POST['password-confirm'] != null) {
+    if (isset($_POST['changepsw'])) {
+        if ($_POST['pwd'] != null && $_POST['password'] != null && $_POST['password-confirm'] != null) {
             $email = $listaInfo[5];
             $op = $_POST['pwd'];
             $np = $_POST['password'];
             $cnp = $_POST['password-confirm'];
-            if(password_verify($op, $listaInfo[6]) && $np == $cnp) {
+            if (password_verify($op, $listaInfo[6]) && $np == $cnp) {
                 $np = password_hash($_POST['password'], PASSWORD_DEFAULT);
                 $connection->updatePsw($email, $np);
             } else {
@@ -67,15 +67,15 @@ if($connectionOk) {
         }
     }
 
-    if($listaInfo != null) {
-        $fname = "<input type=\"text\" name=\"fname\" id=\"fname\" value=\"". $listaInfo[0] ."\" required>";
-        $lname = "<input type=\"text\" name=\"lname\" id=\"lname\" value=\"". $listaInfo[1] ."\" required>";
-        $email = "<input type=\"email\" name=\"email\" id=\"email\" value=\"". $listaInfo[5] ."\" disabled>";
-        $phone = "<input type=\"phone\" name=\"phone\" id=\"phone\" value=\"". $listaInfo[7] ."\" required>";
-        $dob = "<input type=\"date\" name=\"dob\" id=\"dob\" value=\"". $listaInfo[2] ."\" required>";
-        $city = "<input type=\"text\" name=\"city\" id=\"city\" value=\"". $listaInfo[8] ."\" required>";
-        $address = "<input type=\"text\" name=\"address\" id=\"address\" value=\"". $listaInfo[9] ."\" required>";
-        $cap = "<input type=\"text\" name=\"cap\" id=\"cap\" value=\"". $listaInfo[10] ."\" required>";
+    if ($listaInfo != null) {
+        $fname = "<input class=\"dataHolder\" type=\"text\" name=\"fname\" id=\"fname\" value=\"" . $listaInfo[0] . "\" required>";
+        $lname = "<input class=\"dataHolder\" type=\"text\" name=\"lname\" id=\"lname\" value=\"" . $listaInfo[1] . "\" required>";
+        $email = "<input class=\"dataHolder\" type=\"email\" name=\"email\" id=\"email\" value=\"" . $listaInfo[5] . "\" disabled>";
+        $phone = "<input class=\"dataHolder\" type=\"phone\" name=\"phone\" id=\"phone\" value=\"" . $listaInfo[7] . "\" required>";
+        $dob = "<input class=\"dataHolder\" type=\"date\" name=\"dob\" id=\"dob\" value=\"" . $listaInfo[2] . "\" required>";
+        $city = "<input class=\"dataHolder\" type=\"text\" name=\"city\" id=\"city\" value=\"" . $listaInfo[8] . "\" required>";
+        $address = "<input class=\"dataHolder\" type=\"text\" name=\"address\" id=\"address\" value=\"" . $listaInfo[9] . "\" required>";
+        $cap = "<input class=\"dataHolder\" type=\"text\" name=\"cap\" id=\"cap\" value=\"" . $listaInfo[10] . "\" required>";
     }
 }
 /*
@@ -103,6 +103,4 @@ $paginaHTML = str_replace('{address}', $address, $paginaHTML);
 $paginaHTML = str_replace('{cap}', $cap, $paginaHTML);
 
 
-echo($paginaHTML);
-
-?>
+echo ($paginaHTML);
