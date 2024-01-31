75490382f217180ea8f358fbfb5ebc49c1945416
Filippo <rizzolofilippo18@gmail.com>
2024-01-31 22:43:13 +0100

    fixata navbar sulle pagine del desktop

--- src/JS/navbar.js
+++ src/JS/navbar.js
@@ -1,11 +1,22 @@
 // Funzione per la richiesta Fetch
 function fetchDataWithFetch() {
+    // Per Index
     // Effettua la richiesta GET al tuo file PHP
     fetch('src/php/navbar.php')
     .then(response => response.text())
     .then(data => {
         // Gestisci la risposta
         document.getElementById("loginout").innerHTML = data;
+    })
+    .catch(error => console.error('Errore:', error));
+    
+    // Per le altre pagine
+    // Effettua la richiesta GET al tuo file PHP
+    fetch('../php/navbar.php')
+    .then(response => response.text())
+    .then(data => {
+        // Gestisci la risposta
+        document.getElementById("loginoutaltro").innerHTML = data;
     })
     .catch(error => console.error('Errore:', error));
 }
