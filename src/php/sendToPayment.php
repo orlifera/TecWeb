d180e550937b065b37673b715d8aa22494f5c675
Filippo <rizzolofilippo18@gmail.com>
2024-01-31 13:02:17 +0100

    hotfix

--- src/php/sendToPayment.php
+++ src/php/sendToPayment.php
@@ -2,7 +2,7 @@
 session_start();
 if (isset($_POST['processOrder'])) {
     if(isset($_SESSION['user'])) {
-        header('Location: ../pages/payments.html');
+        header('Location: ../php/order.php');
     } else {
         header('Location: ../pages/checkout.html');
     }
